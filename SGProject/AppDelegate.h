46eabaa721a06dcc91d689c4c09777cfaff1815a
huang <huang@Duans-iMac.local>
2013-12-31 13:52:06 +0800

    Initial Commit

---
+++ SGProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SGProject
+//
+//  Created by huang on 12/31/13.
+//  Copyright (c) 2013 SharpGrid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
